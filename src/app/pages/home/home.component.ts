5e2b46ca596396ea1b3050eadfdb665044c9af43
Noha M <33437197+NohaaAa@users.noreply.github.com>
2024-03-17 00:49:29 +0200

    add home content and styles

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,9 +1,24 @@
 import { Component } from '@angular/core';
+import {IconFieldModule} from "primeng/iconfield";
+import {InputIconModule} from "primeng/inputicon";
+import {InputTextModule} from "primeng/inputtext";
+import {InputGroupAddonModule} from "primeng/inputgroupaddon";
+import {InputGroupModule} from "primeng/inputgroup";
+import {ItemCardComponent} from "../../shared/components/item-card/item-card.component";
+import {NgForOf} from "@angular/common";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [
+    IconFieldModule,
+    InputIconModule,
+    InputTextModule,
+    InputGroupAddonModule,
+    InputGroupModule,
+    ItemCardComponent,
+    NgForOf
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
--- src/app/shared/components/item-card/item-card.component.ts
+++ src/app/shared/components/item-card/item-card.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import {NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-item-card',
   standalone: true,
-  imports: [],
+  imports: [
+    NgIf
+  ],
   templateUrl: './item-card.component.html',
-  styleUrl: './item-card.component.css'
+  styleUrl: './item-card.component.scss'
 })
 export class ItemCardComponent {
 
