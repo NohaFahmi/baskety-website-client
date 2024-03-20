9def38b2b2ba12febee45b8df0211e8710b0d73a
Noha M <33437197+NohaaAa@users.noreply.github.com>
2024-03-21 01:05:13 +0200

    add item list section component with diff. cases

--- src/app/shared/components/items-list-section/items-list-section.component.ts
+++ src/app/shared/components/items-list-section/items-list-section.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
+import {ITEM_CARD_MODES, ItemCardComponent} from "../item-card/item-card.component";
+import {NgClass, NgForOf} from "@angular/common";
 
 @Component({
   selector: 'app-items-list-section',
   standalone: true,
-  imports: [],
+  imports: [
+    ItemCardComponent,
+    NgForOf,
+    NgClass
+  ],
   templateUrl: './items-list-section.component.html',
-  styleUrl: './items-list-section.component.css'
+  styleUrl: './items-list-section.component.scss'
 })
 export class ItemsListSectionComponent {
-
+  @Input() itemBtnMode?: ITEM_CARD_MODES;
+  protected readonly ITEM_CARD_MODES = ITEM_CARD_MODES;
 }
