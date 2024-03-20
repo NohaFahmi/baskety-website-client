079d1fe1e4e4bba25919f3f1d7f806baafdb66bc
Noha M <33437197+NohaaAa@users.noreply.github.com>
2024-03-21 01:07:33 +0200

    Enhance component styling and change style files format + add shopping list component UI

--- src/app/shared/components/shopping-list/shopping-list.component.ts
+++ src/app/shared/components/shopping-list/shopping-list.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import {NgOptimizedImage} from "@angular/common";
+import {ButtonModule} from "primeng/button";
+import {ItemsListSectionComponent} from "../items-list-section/items-list-section.component";
+import {InputGroupModule} from "primeng/inputgroup";
+import {InputTextModule} from "primeng/inputtext";
+import {ITEM_CARD_MODES} from "../item-card/item-card.component";
 
 @Component({
   selector: 'app-shopping-list',
   standalone: true,
-  imports: [],
+  imports: [
+    NgOptimizedImage,
+    ButtonModule,
+    ItemsListSectionComponent,
+    InputGroupModule,
+    InputTextModule
+  ],
   templateUrl: './shopping-list.component.html',
-  styleUrl: './shopping-list.component.css'
+  styleUrl: './shopping-list.component.scss'
 })
 export class ShoppingListComponent {
 
+  protected readonly ITEM_CARD_MODES = ITEM_CARD_MODES;
 }
