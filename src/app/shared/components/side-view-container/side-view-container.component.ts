1a906b5658b8fc716df34d2d0f8ed6dcb8b9e47f
Noha M <33437197+NohaaAa@users.noreply.github.com>
2024-03-21 01:05:56 +0200

    add side view container component

--- src/app/shared/components/side-view-container/side-view-container.component.ts
+++ src/app/shared/components/side-view-container/side-view-container.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import {SidebarModule} from "primeng/sidebar";
 import {ButtonModule} from "primeng/button";
+import {ShoppingListComponent} from "../shopping-list/shopping-list.component";
 
 @Component({
   selector: 'app-side-view-container',
   standalone: true,
   imports: [
     SidebarModule,
-    ButtonModule
+    ButtonModule,
+    ShoppingListComponent
   ],
   templateUrl: './side-view-container.component.html',
   styleUrl: './side-view-container.component.scss'
