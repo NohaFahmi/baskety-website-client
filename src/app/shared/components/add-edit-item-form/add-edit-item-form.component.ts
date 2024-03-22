d06a6ce1a36b00d9b90dfb32b6cf5f3136458c40
Noha M <33437197+NohaaAa@users.noreply.github.com>
2024-03-22 16:23:46 +0200

    Create add-edit-item form and style updates

--- src/app/shared/components/add-edit-item-form/add-edit-item-form.component.ts
+++ src/app/shared/components/add-edit-item-form/add-edit-item-form.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
+import {InputTextModule} from "primeng/inputtext";
+import {InputTextareaModule} from "primeng/inputtextarea";
+import {FileUploadModule} from "primeng/fileupload";
+import {DropdownModule} from "primeng/dropdown";
 
 @Component({
   selector: 'app-add-edit-item-form',
   standalone: true,
-  imports: [],
+  imports: [
+    InputTextModule,
+    InputTextareaModule,
+    FileUploadModule,
+    DropdownModule
+  ],
   templateUrl: './add-edit-item-form.component.html',
   styleUrl: './add-edit-item-form.component.scss'
 })
 export class AddEditItemFormComponent {
-
+  @Input() isEdit = false;
 }
