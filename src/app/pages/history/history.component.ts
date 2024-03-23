55ae8288461c02490603c62e0228e83fbbff60ae
Noha M <33437197+NohaaAa@users.noreply.github.com>
2024-03-23 13:58:19 +0200

    add history page UI

--- src/app/pages/history/history.component.ts
+++ src/app/pages/history/history.component.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
+import {CardModule} from "primeng/card";
+import {ButtonModule} from "primeng/button";
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-history',
   standalone: true,
-  imports: [],
+  imports: [
+    CardModule,
+    ButtonModule,
+    RouterLink
+  ],
   templateUrl: './history.component.html',
   styleUrl: './history.component.scss'
 })
