0c1df76dac775cd2f1e52af56d57c03d21127abc
Noha M <33437197+NohaaAa@users.noreply.github.com>
2024-03-23 04:12:56 +0200

    Add detailed view and controls for item details

--- src/app/shared/components/item-details/item-details.component.ts
+++ src/app/shared/components/item-details/item-details.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import {ButtonModule} from "primeng/button";
+import {NgOptimizedImage} from "@angular/common";
 
 @Component({
   selector: 'app-item-details',
   standalone: true,
-  imports: [],
+  imports: [
+    ButtonModule,
+    NgOptimizedImage
+  ],
   templateUrl: './item-details.component.html',
   styleUrl: './item-details.component.scss'
 })
