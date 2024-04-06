e07e5027b271aa751724d2e89f79c1e5022115ce
Noha M <33437197+NohaaAa@users.noreply.github.com>
2024-04-06 23:27:34 +0200

    "Added user operations in UserService"

--- src/app/core/services/user/user.service.ts
+++ src/app/core/services/user/user.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import {IUser} from "../../../shared/interfaces/user.interface";
+import {firstValueFrom, Observable} from "rxjs";
+import {HttpService} from "../http/http.service";
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  constructor(private httpService: HttpService) { }
+
+  saveUserInDB(userInfo: IUser): Promise<IUser> {
+    return firstValueFrom(this.httpService.post('user', userInfo));
+  }
+
+  getUserFromDBByUid(user: IUser): Promise<any> {
+    return firstValueFrom(this.httpService.get(`user/byUid/${user.uid}`));
+  }
+  updateUserInDB(userId: string, userInfo: IUser): Promise<any> {
+    return firstValueFrom(this.httpService.put(`user/${userId}`, userInfo));
+  }
 }
---
+++ src/app/shared/interfaces/user.interface.ts
@@ -0,0 +1,7 @@
+export interface IUser {
+  uid: string;
+  email: string;
+  username: string;
+  photoURL: string;
+  emailVerified: boolean;
+}
