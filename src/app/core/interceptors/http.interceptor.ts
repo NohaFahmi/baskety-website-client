423a115598d79e199bcf421e49d6256c260477d1
Noha M <33437197+NohaaAa@users.noreply.github.com>
2024-04-20 20:38:37 +0200

    update interceptor

--- src/app/core/interceptors/http.interceptor.ts
+++ src/app/core/interceptors/http.interceptor.ts
@@ -1,18 +1,28 @@
-import { HttpInterceptorFn } from '@angular/common/http';
+import {HttpHandler, HttpInterceptorFn, HttpRequest} from '@angular/common/http';
 import {AuthService} from "../services/auth/auth.service";
 import {inject} from "@angular/core";
+import {Auth} from "@angular/fire/auth";
+import {from, Observable, switchMap} from "rxjs";
 
 export const httpInterceptor: HttpInterceptorFn = (req, next) => {
   if (req.url.includes('api/v1/user') && req.method === 'POST') {
     return next(req);
   } else {
     const authService = inject(AuthService);
-    const authToken = authService.getAccessToken();
-    const authReq = req.clone({
-      setHeaders: {
-        Authorization: `Bearer ${authToken}`
-      }
-    });
-    return next(authReq);
+    const auth = inject(Auth);
+    if (auth.currentUser) {
+      return from(auth.currentUser.getIdToken(true)).pipe(
+        switchMap(idToken => {
+          const authReq = req.clone({
+            setHeaders: {
+              Authorization: `Bearer ${idToken}`
+            }
+          });
+          return next(authReq);
+        })
+      );
+    } else {
+      return next(req);
+    }
   }
 };
