a04e7395f5277f381b1ed742005dfee80c47eb41
Noha M <33437197+NohaaAa@users.noreply.github.com>
2024-04-20 16:26:11 +0200

    implement the integration of add/edit item form

--- src/app/shared/components/add-edit-item-form/add-edit-item-form.component.ts
+++ src/app/shared/components/add-edit-item-form/add-edit-item-form.component.ts
@@ -1,8 +1,15 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, signal} from '@angular/core';
 import {InputTextModule} from "primeng/inputtext";
 import {InputTextareaModule} from "primeng/inputtextarea";
 import {FileUploadModule} from "primeng/fileupload";
 import {DropdownModule} from "primeng/dropdown";
+import {CategoryService} from "../../../core/services/category/category.service";
+import {ICategory} from "../../interfaces/category.interface";
+import {NgIf} from "@angular/common";
+import {FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
+import {getFormValidationErrors} from "../../helpers/helpers";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {ItemService} from "../../../core/services/item/item.service";
 
 @Component({
   selector: 'app-add-edit-item-form',
@@ -11,11 +18,70 @@ import {DropdownModule} from "primeng/dropdown";
     InputTextModule,
     InputTextareaModule,
     FileUploadModule,
-    DropdownModule
+    DropdownModule,
+    NgIf,
+    ReactiveFormsModule
   ],
   templateUrl: './add-edit-item-form.component.html',
-  styleUrl: './add-edit-item-form.component.scss'
+  styleUrl: './add-edit-item-form.component.scss',
 })
-export class AddEditItemFormComponent {
+export class AddEditItemFormComponent{
   @Input() isEdit = false;
+  categoriesList: ICategory[] = [];
+  itemForm: FormGroup;
+  isLoading = false;
+  protected readonly getFormValidationErrors = getFormValidationErrors;
+
+  constructor(private categoriesService: CategoryService,
+              private itemService: ItemService,
+              private fb: FormBuilder) {
+    this.itemForm = this.fb.group({
+      name: new FormControl('', Validators.required),
+      description: new FormControl('', Validators.required),
+      // imageURL: new FormControl('', Validators.required),
+      price: new FormControl(0, Validators.required),
+      quantity: new  FormControl(0, Validators.required),
+      category: new FormControl('', Validators.required),
+      quantityUnit: new FormControl('', Validators.required)
+    })
+  }
+  ngOnInit() {
+      this.loadCategories();
+  }
+
+  loadCategories() {
+    this.categoriesService.gatAllCategories().then((categories) => {
+      this.categoriesList = categories;
+    });
+  }
+  // uploadCategoryImg(event: any): void {
+  //   console.log('here', event);
+  //   const driveUrl = 'https://www.googleapis.com/drive/v3/files';
+  //   let formData = new FormData();
+  //   formData.append('metadata', new Blob([JSON.stringify({
+  //     'name': event.name,
+  //     'mimeType': event.type
+  //   })], {type: 'application/json'}));
+  //   formData.append('file', event);
+  //
+  //   let headers = new HttpHeaders({
+  //     'Authorization': 'Bearer ' + '[Your OAuth token]',
+  //     'Content-Type': ': multipart/related',
+  //   });
+  //   this.httpService.post(`${driveUrl}?uploadType=multipart`, formData, { headers });
+  // }
+  onItemCreation(): void {
+    console.log(this.itemForm.value);
+    this.isLoading = true;
+    this.itemService.createItem(this.itemForm.value).then((item) => {
+      console.log('item', item);
+      this.itemForm.reset();
+    }).catch((error) => {
+      console.log('erer', error);
+    }).finally(() => {this.isLoading = false});
+  }
+
+  onCancel(): void {
+    this.itemForm.reset();
+  }
 }
