5fb35d6305727e45c3127ad3840bddbb6cfb1254
Noha M <33437197+NohaaAa@users.noreply.github.com>
2024-05-03 19:07:24 +0300

    Improve item addition logic in shopping list service

--- src/app/shared/services/shopping-list/shopping-list.service.ts
+++ src/app/shared/services/shopping-list/shopping-list.service.ts
@@ -1,8 +1,7 @@
 import {Injectable, signal} from '@angular/core';
 import {ListService} from "../../../core/services/list/list.service";
 import {IItem} from "../../interfaces/item.interface";
-import {IList, ShoppingListStatus} from "../../interfaces/list.interface";
-import {error} from "@angular/compiler-cli/src/transformers/util";
+import {IList, IListReq, ShoppingListStatus} from "../../interfaces/list.interface";
 import {CategoryService} from "../../../core/services/category/category.service";
 
 @Injectable({
@@ -15,11 +14,35 @@ export class ShoppingListService {
               private categoryService: CategoryService) { }
 
   addItemToShoppingList(item: IItem): void {
-    if (!this.currentShoppingList()) {
-      const list: IList = {
+    if (this.currentShoppingList() && this.currentShoppingList()?.items) {
+      const currListItems = this.currentShoppingList()?.items ?? [];
+      const currListItemsIds: Set<any> = new Set(currListItems.map((item) => item._id ?? ''));
+      if(currListItemsIds.has(item._id)) {
+        console.log('item already added in list');
+      } else {
+        currListItemsIds.add(item._id ?? '');
+        const updatedList: IListReq = {
+          items: Array.from(currListItemsIds),
+          _id: this.currentShoppingList()?._id,
+          status: this.currentShoppingList()?.status,
+          name: this.currentShoppingList()?.name,
+          userId: this.currentShoppingList()?.userId,
+          completedAt: this.currentShoppingList()?.completedAt
+        }
+        this.listService.updateList(this.currentShoppingList()?._id ?? '', updatedList).then((list) => {
+          this.getCurrentShoppingList();
+        }).catch((error) => {
+          console.log('error');
+        })
+      }
+
+    } else {
+      const items = [];
+      items.push(item._id ?? '');
+      const list: IListReq = {
         name: 'new list',
         status: ShoppingListStatus.OPEN,
-        items: [item]
+        items
       }
       this.listService.createList(list).then((list) => {
         this.currentShoppingList.set(list);
