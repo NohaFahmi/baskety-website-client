efb9001c8bf330ef67e5db334254623d05465878
Noha M. <33437197+NohaaAa@users.noreply.github.com>
2025-01-22 21:32:21 +0200

    Refactor SideViewsService to remove unused functionality.
    Commented out signals, methods, and dependencies that are not in use to simplify the SideViewsService. This cleanup improves code maintainability and readability by reducing unnecessary complexity.

--- src/app/shared/services/side-views/side-views.service.ts
+++ src/app/shared/services/side-views/side-views.service.ts
@@ -1,61 +1,55 @@
-import {Injectable, signal} from '@angular/core';
-import {SIDENAV_VIEWS} from "../../interfaces/common.interface";
-import {IItem} from "../../interfaces/item.interface";
-import {ItemService} from "../../../core/services/item/item.service";
-import {ListService} from "../../../core/services/list/list.service";
-import {IList} from "../../interfaces/list.interface";
-import {ShoppingListService} from "../shopping-list/shopping-list.service";
+import {Injectable} from '@angular/core';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class SideViewsService {
-  currentView = signal<SIDENAV_VIEWS>(SIDENAV_VIEWS.SHOPPING_LIST);
-  itemDetails = signal<IItem | null>(null);
-  isSideViewOpen = signal<boolean>(true);
-  isEditItemMode = signal<boolean>(false);
-  constructor(private itemService: ItemService,
-              private shoppingListService: ShoppingListService) { }
+  // currentView = signal<SIDENAV_VIEWS>(SIDENAV_VIEWS.SHOPPING_LIST);
+  // itemDetails = signal<IItem | null>(null);
+  // isSideViewOpen = signal<boolean>(true);
+  // isEditItemMode = signal<boolean>(false);
 
-  updateCurrentSideView(view: SIDENAV_VIEWS): void {
-    this.itemDetails.set(null);
-    this.isEditItemMode.set(false);
-    // this.shoppingListService.getCurrentShoppingList();
-    this.currentView.set(view);
-  }
 
-  navigateToItemDetailsView(itemId: string): void {
-    if (itemId) {
-      // this.shoppingListService.getCurrentShoppingList();
-      this.itemService.getItemById(itemId).then((details) => {
-        this.currentView.set(SIDENAV_VIEWS.ITEM_DETAILS);
-        this.itemDetails.set(details);
-        if (!this.isSideViewOpen()) {
-          this.toggleDisplaySideView();
-        }
-      });
-    }
-  }
+  // updateCurrentSideView(view: SIDENAV_VIEWS): void {
+  //   this.itemDetails.set(null);
+  //   this.isEditItemMode.set(false);
+  //   // this.shoppingListService.getCurrentShoppingList();
+  //   this.currentView.set(view);
+  // }
 
-  navigateToEditItemView(itemId: string): void {
-    if (itemId) {
-      // this.shoppingListService.getCurrentShoppingList();
-      this.itemService.getItemById(itemId).then((details) => {
-        this.currentView.set(SIDENAV_VIEWS.ADD_EDIT_ITEM);
-        this.itemDetails.set(details);
-        this.isEditItemMode.set(true);
-        if (!this.isSideViewOpen()) {
-          this.toggleDisplaySideView();
-        }
-      })
-    }
-  }
+  // navigateToItemDetailsView(itemId: number): void {
+  //   if (itemId) {
+  //     // this.shoppingListService.getCurrentShoppingList();
+  //     this.itemService.getItemById(itemId).then((details) => {
+  //       this.currentView.set(SIDENAV_VIEWS.ITEM_DETAILS);
+  //       this.itemDetails.set(details);
+  //       if (!this.isSideViewOpen()) {
+  //         this.toggleDisplaySideView();
+  //       }
+  //     });
+  //   }
+  // }
 
-  toggleDisplaySideView(): void {
-    this.isSideViewOpen.update((val) => !val);
-    if(this.isSideViewOpen()) {
-      // this.shoppingListService.getCurrentShoppingList();
-    }
-  }
+  // navigateToEditItemView(itemId: number): void {
+  //   if (itemId) {
+  //     // this.shoppingListService.getCurrentShoppingList();
+  //     this.itemService.getItemById(itemId).then((details) => {
+  //       this.currentView.set(SIDENAV_VIEWS.ADD_EDIT_ITEM);
+  //       this.itemDetails.set(details);
+  //       this.isEditItemMode.set(true);
+  //       if (!this.isSideViewOpen()) {
+  //         this.toggleDisplaySideView();
+  //       }
+  //     })
+  //   }
+  // }
+
+  // toggleDisplaySideView(): void {
+  //   this.isSideViewOpen.update((val) => !val);
+  //   if(this.isSideViewOpen()) {
+  //     // this.shoppingListService.getCurrentShoppingList();
+  //   }
+  // }
 
 }
